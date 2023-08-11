3c95b6dec89d3bac336ab3892885501db452c77c
archana8281 <archanasanthoshp@gmail.com>
2023-08-11 16:33:01 +0530

    changed

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $selectdp = mysqli_fetch_assoc($result) or die("query error");
             </div>
             <div>
                <form id="uploadData" action="upload.php" method="post" enctype="multipart/form-data">
-                  <input type="file" name="addNew" id="addNew" accept="image/*" style="display: none;">
+                  <input type="file" name="addNew" id="addNew"  style="display: none;">
                   <i class="fa fa-plus" aria-hidden="true" id="add"></i>
                </form>
             </div>
